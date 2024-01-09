c6e0a2d5d65a9e8b1dd7ad8802d336327a021140
Jesse Okoro <okorojesse@gmail.com>
2024-01-09 00:24:37 +0000

    multiple returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    first_char = sentence[0] if len(sentencei) > 0 else "None"
+    first_char = sentence[0] if len(sentence) > 0 else "None"
     tup = len(sentence), first_char
     return tup

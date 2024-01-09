ade096c6d9e5a284ea641b381efec9ee7a58dcce
Jesse Okoro <okorojesse@gmail.com>
2024-01-09 00:14:28 +0000

    tuples addition

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -5,3 +5,4 @@ def print_reversed_list_integer(my_list=[]):
 def new_in_list(my_list, idx, element):
 def no_c(my_string):
 def print_matrix_integer(matrix=[[]]):
+def add_tuple(tuple_a=(), tuple_b=()):
